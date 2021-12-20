57b2a86882d7b5104d3a28beec20173ac9d1726e
ymd65536 <ymd65536@gmail.com>
2021-12-21 08:57:11 +0900

    クライアント領域を変更

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -17,7 +17,7 @@
     {
       this.components = new System.ComponentModel.Container();
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(800, 500);
       this.Text = "IEっぽいやつ";
     }
   }
