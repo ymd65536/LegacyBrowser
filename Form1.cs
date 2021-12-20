e78043ac9612e9a5b44017aa3d8735362f0886af
Kento.Yamada <ymd65536@gmail.com>
2021-12-21 00:12:13 +0900

    FormClosingEventHandler に変更

--- Form1.cs
+++ Form1.cs
@@ -1,43 +1,45 @@
 ﻿using System;
 using System.Windows.Forms;
 using WebBrowserExtended;
+using NewWebWin;
 
 namespace WebForms
 {
-  public partial class WebForm : Form
-  {
-    private MyWebBrowser Browser;
-    private WebForm frmWB;
+    public partial class WebForm : Form
+    {
+        private MyWebBrowser Browser;
+        private WebForm frmWB;
 
-    public WebForm()
-    {
-      Browser = new MyWebBrowser();
-      Browser.ScriptErrorsSuppressed = true;
-      Browser.Navigate("C:\\web_forms\\index.html");
-      InitializeComponent();
+        public WebForm()
+        {
+            Browser = new MyWebBrowser();
+            Browser.ScriptErrorsSuppressed = true;
+            Browser.Navigate("https://www.google.com/?hl=ja");
+            InitializeComponent();
 
-      Browser.Dock = DockStyle.Fill;
-      this.Controls.Add(Browser);
-      Browser.NewWindow2 += new NewWindow2EventHandler(Browser_NewWindow2);
-      Browser.Closing += new MyWebBrowser.ClosingEventHandler(OnQuit);
+            Browser.Dock = DockStyle.Fill;
+            this.Controls.Add(Browser);
+            Browser.NewWindow2 += new NewWindow2EventHandler(Browser_NewWindow2);
+            Browser.Closing += new MyWebBrowser.FormClosingEventHandler(OnQuit);
+        }
+        private void OnQuit(object sender, EventArgs e)
+        {
+            this.Browser.RegisterAsBrowser = false;
+            this.Browser.Dispose();
+            Browser = null;
+            this.Close();
+        }
+        private void Browser_NewWindow2(object sender, NewWindow2EventArgs e)
+        {
+            // WebBrowser.AttachInterfaces メソッドは Visible
+            // プロパティを true にすると呼び出される。なので、
+            // ここで設定しないと RegisterAsBrowser プロパティ、
+            // Application プロパティで例外がスローされてしまう。
+            frmWB = new WebForm();
+            frmWB.Visible = true;
+            frmWB.Browser.ScriptErrorsSuppressed = true;
+            frmWB.Browser.RegisterAsBrowser = true;
+            e.PpDisp = frmWB.Browser.Application;
+        }
     }
-    private void OnQuit(object sender, EventArgs e)
-    {
-      this.Browser.Dispose();
-      Browser = null;
-      this.Close();
-    }
-    private void Browser_NewWindow2(object sender, NewWindow2EventArgs e)
-    {
-      // WebBrowser.AttachInterfaces メソッドは Visible
-      // プロパティを true にすると呼び出される。なので、
-      // ここで設定しないと RegisterAsBrowser プロパティ、
-      // Application プロパティで例外がスローされてしまう。
-      frmWB = new WebForm();
-      frmWB.Visible = true;
-      frmWB.Browser.ScriptErrorsSuppressed = true;
-      frmWB.Browser.RegisterAsBrowser = true;
-      e.PpDisp = frmWB.Browser.Application;
-    }
-  }
 }
